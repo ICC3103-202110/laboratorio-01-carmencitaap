bbe1caa01a8c93be2165d07527ea9afcb2415b06
carmencitaap <cmavendano@miuandes.cl>
2021-03-26 18:22:26 -0300

    final commit

--- lab1_oop.py
+++ lab1_oop.py
@@ -12,33 +12,30 @@ num_cards2 = num_cards
 print("Player 1 starts")
 
 while lost == 1: 
-    gano = 0
+    
     deck = []
     deck1 = []
     for i in range(num_cards):
-    #print(num_cartas)
+    #print(num_cards)
         num_cards -= 1
         deck.append(num_cards+1)
         deck1.append(num_cards+1)
         random.shuffle(deck) #2
         random.shuffle(deck1) #2
     #gano = 0
-    print(deck)
-    print(deck1)
-    tablero= [deck,deck1]
+    #print(deck)  #with these prints (both of them) you can see the board, to test the game. 
+    #print(deck1)
+
     p_j1 = 0 #3
     p_j2 = 0 #3
     
     
     hidden_board1 = []
     hidden_board2 = []
-    fila = []
 
     for k in range(num_cards2):
         hidden_board1.append("*")
         hidden_board2.append("*")
-#print(tablero_oculto)
-
     tab = ""
     tab2 = ""
     for a in range(len(hidden_board1)):
@@ -48,16 +45,16 @@ while lost == 1:
     
     print(tab)
     print(tab2)
-    
-    #gana = 1
+    lost = 0
+    #lost = 0
     while won == 1:
-        #gano = 0
-        cd = input("Enter a coordenate to flip one card (Pick one from each deck): ") #5
+        lost = 0
+        cd = input("Enter a coordenate to flip one card \n(Pick one from each deck): ") #5
         cd.split(",")
         c1 = int(cd[0])
         c2 = int(cd[2])-1
 
-#carta1
+#flip card 1
         if c1 == 1:
             hidden_board1.pop(c2)
             card1 = int(deck[c2])
@@ -68,8 +65,8 @@ while lost == 1:
             card1  = int(deck1[c2])
             hidden_board2.insert(c2,card1)
     
-#carta2
-        coord = input("Enter a coordenate to flip another card (Pick one from the deck you haven't chosen): ")
+#flip card 2
+        coord = input("Enter a coordenate to flip another card \n(Pick one from the deck you haven't chosen): ")
         coord.split(",")
         coord1 = int(coord[0])
         coord2 = int(coord[2])-1
@@ -85,7 +82,6 @@ while lost == 1:
 
         print(hidden_board1)
         print(hidden_board2)
-        #gano = 0
         if card1 == card2:
             p_j1 += 1
             print("Keep playing! You chose the right pair")
@@ -96,6 +92,7 @@ while lost == 1:
             if coord1 == 2:
                 hidden_board2.pop(coord2)
                 hidden_board2.insert(coord2, "")
+                
             end = hidden_board1.count("")
             end1 = hidden_board2.count("")
             won = 1
@@ -103,20 +100,11 @@ while lost == 1:
             print(hidden_board2)
             if end == num_cards2 and end == end1:
                 print("You've won!")
-                lose = 0
+                lost = 1
                 won = 0 
-            
-            #gano = 0
-            
-        else:
-            print("Now player 2 plays")
-            won = 0
-            #lost = 1
-    #gano = 0
-    
-       # if carta1 == carta2:
-           #p_j2 += 1
-        
-    
-    #print(p_j1)
 
+        if card1 != card2:
+            print("You've lost, now player 2 gets to play")
+        won = 0
+        lost = 1
+    lost = 0
